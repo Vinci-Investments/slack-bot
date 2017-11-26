52a11fd4623b3728e78a906e030092992cfc81dd
Maxence Coupet <maxence.coupet@gmail.com>
2017-11-26 18:23:41 +0100

    Update slack.py

--- slack.py
+++ slack.py
@@ -149,7 +149,7 @@ def job():
         else:
             print("Connection failed. Invalid Slack token or bot ID?")
 
-schedule.every().day.at("18:20").do(job)
+schedule.every().day.at("12:26").do(job)
 
 while 1:
     schedule.run_pending()
